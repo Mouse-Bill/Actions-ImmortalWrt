52e4bd12928d90171f6e06d1481e795aba05ef56
Mouse-Bill <46294220+Mouse-Bill@users.noreply.github.com>
2024-11-18 21:13:23 +0800

    Update diy.sh mosdns

--- diy.sh
+++ diy.sh
@@ -2,3 +2,9 @@
 # Please enter the custom command below (usually used to install third-party plugins) (can be left blank)
 # git clone --depth=1 https://github.com/EOYOHOO/UA2F.git package/UA2F
 # git clone --depth=1 https://github.com/EOYOHOO/rkp-ipid.git package/rkp-ipid
+
+find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
+find ./ | grep Makefile | grep mosdns | xargs rm -f
+
+git clone --depth=1 https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone --depth=1 https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
