ad6927be4990eec45739a6d9f2153932e603ba91
Mouse-Bill <46294220+Mouse-Bill@users.noreply.github.com>
2024-11-19 16:42:59 +0800

    Update diy1.sh bump go 1.23

--- diy1.sh
+++ diy1.sh
@@ -3,3 +3,6 @@
 
 #git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 #git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
