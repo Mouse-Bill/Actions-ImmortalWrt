d46304bcd69145c00ffb8c59ce39568f86988cc5
Mouse-Bill <46294220+Mouse-Bill@users.noreply.github.com>
2024-11-19 23:12:22 +0800

    Update diy1.sh upx speedtest-web

--- diy1.sh
+++ diy1.sh
@@ -6,3 +6,5 @@
 
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
+
+sed -i '/^PKG_BUILD_DEPENDS:=/ !b; /upx\/host/ b; s/$/ upx\/host/' ./feeds/smpackage/speedtest-web/Makefile 
