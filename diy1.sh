7fd943f017592270eab67c52382ce3e4815f99e3
Mouse-Bill <46294220+Mouse-Bill@users.noreply.github.com>
2024-11-20 02:23:29 +0800

    Update diy1.sh hostpkg

--- diy1.sh
+++ diy1.sh
@@ -8,3 +8,5 @@ rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
 
 sed -i '/^PKG_BUILD_DEPENDS:=/ !b; /upx\/host/ b; s/$/ upx\/host/' ./feeds/smpackage/speedtest-web/Makefile 
+sed -i 's#\$(STAGING_DIR_HOST)/bin/upx#\$(STAGING_DIR_HOST)/hostpkg/bin/upx#' ./feeds/smpackage/speedtest-web/Makefile 
+
